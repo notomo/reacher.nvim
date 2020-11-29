b317ee079ef8c7df842592965de5b9340010b200
notomo <notomo.motono@gmail.com>
2020-11-29 15:37:53 +0900

    Refactor origin

---
+++ lua/reacher/view/origin.lua
@@ -0,0 +1,83 @@
+local M = {}
+
+local Origin = {}
+Origin.__index = Origin
+M.Origin = Origin
+
+function Origin.new(bufnr)
+  local first_row = vim.fn.line("w0")
+  local last_row = vim.fn.line("w$")
+  local lines = vim.api.nvim_buf_get_lines(bufnr, first_row - 1, last_row, true)
+
+  local options = {
+    list = vim.wo.list,
+    wrap = vim.wo.wrap,
+    textwidth = vim.bo.textwidth,
+    listchars = vim.o.listchars,
+  }
+
+  local id = vim.api.nvim_get_current_win()
+
+  local cursor_row, cursor_column = unpack(vim.api.nvim_win_get_cursor(id))
+  local saved = vim.fn.winsaveview()
+  vim.api.nvim_win_set_cursor(id, {cursor_row, 0})
+  local number_sign_width = vim.fn.wincol()
+  vim.fn.winrestview(saved)
+
+  local width = vim.api.nvim_win_get_width(id) - number_sign_width + 1
+  local height = vim.api.nvim_win_get_height(id)
+
+  local row = 0
+  local column = 0
+  local first_column = saved.leftcol
+  if first_column >= number_sign_width then
+    column = number_sign_width + 1
+  elseif first_column >= 1 then
+    column = first_column
+  end
+
+  local config = vim.api.nvim_win_get_config(id)
+  if config.relative ~= "" then
+    row = row + config.row[false]
+    column = column + config.col[false]
+  end
+
+  local tbl = {
+    id = id,
+    row = row,
+    column = column,
+    width = width,
+    height = height,
+    first_row = first_row,
+    last_row = last_row,
+    cursor = {row = cursor_row, column = cursor_column},
+    _saved = saved,
+    _lines = lines,
+    _options = options,
+  }
+  return setmetatable(tbl, Origin)
+end
+
+function Origin.copy(self, bufnr, window_id)
+  vim.fn.winrestview({
+    col = self._saved.col,
+    coladd = self._saved.coladd,
+    leftcol = self._saved.leftcol,
+    skipcol = self._saved.skipcol,
+  })
+
+  vim.api.nvim_buf_set_lines(bufnr, 0, -1, true, self._lines)
+  vim.api.nvim_buf_set_option(bufnr, "textwidth", self._options.textwidth)
+  vim.api.nvim_win_set_cursor(window_id, {
+    self.cursor.row - (self.first_row - 1),
+    self.cursor.column,
+  })
+  vim.api.nvim_win_set_option(window_id, "list", self._options.list)
+  vim.api.nvim_win_set_option(window_id, "wrap", self._options.wrap)
+
+  local listchars = table.concat(vim.fn.split(self._options.listchars, ",precedes:."))
+  listchars = table.concat(vim.split(listchars, ",extends:.", true))
+  vim.api.nvim_win_set_option(window_id, "listchars", listchars)
+end
+
+return M
--- lua/reacher/view/overlay.lua
+++ lua/reacher/view/overlay.lua
@@ -1,4 +1,5 @@
 local windowlib = require("reacher/lib/window")
+local Origin = require("reacher/view/origin").Origin
 
 local M = {}
 
@@ -7,89 +8,35 @@ Overlay.__index = Overlay
 M.Overlay = Overlay
 
 function Overlay.open(source, source_bufnr)
-  local first_row = vim.fn.line("w0")
-  local last_row = vim.fn.line("w$")
-  local lines = vim.api.nvim_buf_get_lines(source_bufnr, first_row - 1, last_row, true)
-
-  local original = {
-    list = vim.wo.list,
-    wrap = vim.wo.wrap,
-    textwidth = vim.bo.textwidth,
-    listchars = vim.o.listchars,
-  }
-
-  local origin_window_id = vim.api.nvim_get_current_win()
-
-  local cursor_row, cursor_column = unpack(vim.api.nvim_win_get_cursor(origin_window_id))
-  local view = vim.fn.winsaveview()
-  local restore_view = function()
-    vim.fn.winrestview(view)
-  end
-  vim.api.nvim_win_set_cursor(origin_window_id, {cursor_row, 0})
-  local number_sign_width = vim.fn.wincol()
-  restore_view()
-
-  local width = vim.api.nvim_win_get_width(origin_window_id) - number_sign_width + 1
-  local height = vim.api.nvim_win_get_height(origin_window_id)
-
-  local row = 0
-  local column = 0
-  local first_column = view.leftcol
-  if first_column >= number_sign_width then
-    column = number_sign_width + 1
-  elseif first_column >= 1 then
-    column = first_column
-  end
-
-  local win_config = vim.api.nvim_win_get_config(origin_window_id)
-  if win_config.relative ~= "" then
-    row = row + win_config.row[false]
-    column = column + win_config.col[false]
-  end
+  local origin = Origin.new(source_bufnr)
 
   local bufnr = vim.api.nvim_create_buf(false, true)
   local window_id = vim.api.nvim_open_win(bufnr, true, {
-    width = width,
-    height = height,
+    width = origin.width,
+    height = origin.height,
     relative = "win",
-    row = row,
-    col = column,
-    bufpos = {first_row - 1, 0},
+    row = origin.row,
+    col = origin.column,
+    bufpos = {origin.first_row - 1, 0},
     external = false,
     style = "minimal",
   })
-  vim.fn.winrestview({
-    col = view.col,
-    coladd = view.coladd,
-    leftcol = view.leftcol,
-    skipcol = view.skipcol,
-  })
+  origin:copy(bufnr, window_id)
 
-  vim.api.nvim_buf_set_lines(bufnr, 0, -1, true, lines)
-  vim.api.nvim_win_set_cursor(window_id, {cursor_row - (first_row - 1), cursor_column})
   vim.api.nvim_buf_set_option(bufnr, "bufhidden", "wipe")
   vim.api.nvim_buf_set_option(bufnr, "modifiable", false)
-  vim.api.nvim_buf_set_option(bufnr, "textwidth", original.textwidth)
-
   vim.api.nvim_win_set_option(window_id, "winhighlight", "Normal:ReacherBackground")
-  vim.api.nvim_win_set_option(window_id, "list", original.list)
-  vim.api.nvim_win_set_option(window_id, "wrap", original.wrap)
 
-  local listchars = table.concat(vim.fn.split(original.listchars, ",precedes:."))
-  listchars = table.concat(vim.split(listchars, ",extends:.", true))
-  vim.api.nvim_win_set_option(window_id, "listchars", listchars)
+  local positions = source.collect(source_bufnr, origin.first_row, origin.last_row)
 
-  local positions = source.collect(source_bufnr, first_row, last_row)
-
-  local cursor = vim.api.nvim_win_get_cursor(origin_window_id)
   local tbl = {
     window_id = window_id,
-    _origin_window_id = origin_window_id,
+    _origin_window_id = origin.id,
     _bufnr = bufnr,
-    _row_offset = first_row - 1,
+    _row_offset = origin.first_row - 1,
     _all_positions = positions,
     _positions = {},
-    _cursor = {row = cursor[1], column = cursor[2]},
+    _cursor = origin.cursor,
   }
   local overlay = setmetatable(tbl, Overlay)
 
@@ -156,7 +103,7 @@ function Overlay.update(self, input_line)
       hl_pos = {row = pos.row, column = pos.column, y_diff = y_diff, x_diff = x_diff}
       self._index = i
     end
-    vim.api.nvim_buf_add_highlight(self._bufnr, ns, "String", pos.row - self._row_offset - 1, pos.column, pos.column + 1)
+    vim.api.nvim_buf_add_highlight(self._bufnr, ns, "WarningMsg", pos.row - self._row_offset - 1, pos.column, pos.column + 1)
   end
   vim.api.nvim_buf_add_highlight(self._bufnr, current_ns, "Todo", hl_pos.row - self._row_offset - 1, hl_pos.column, hl_pos.column + 1)
 
