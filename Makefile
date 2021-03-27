8a08248f03769e7af2eaf7dc1c3fa290a7dd165a
notomo <notomo.motono@gmail.com>
2021-03-27 11:01:17 +0900

    Add virtes scenario

--- Makefile
+++ Makefile
@@ -1,3 +1,11 @@
 test:
 	vusted --shuffle
 .PHONY: test
+
+COMPARE_HASH := HEAD^
+RESULT_DIR := ./spec/lua/reacher/virtes/screenshot
+diff_screenshot:
+	nvim --clean -n +"lua dofile('./spec/lua/reacher/virtes/scenario.lua')('${COMPARE_HASH}', '${RESULT_DIR}')" +"quitall!"
+	cat ${RESULT_DIR}/replay.vim
+	test ! -s ${RESULT_DIR}/replay.vim
+.PHONY: diff_screenshot
---
+++ spec/lua/reacher/virtes/scenario.lua
@@ -0,0 +1,49 @@
+local reacher = setmetatable({}, {
+  __index = function(_, k)
+    return require("reacher")[k]
+  end,
+})
+
+local scenario = function(ctx)
+  vim.o.termguicolors = true
+  vim.o.background = "dark"
+  vim.o.hlsearch = false
+
+  vim.api.nvim_buf_set_lines(0, 0, -1, false, vim.split([[
+hoge
+hoge
+hoge
+]], "\n", true))
+
+  reacher.start({input = "hoge"})
+  ctx:screenshot()
+
+  reacher.next()
+  ctx:screenshot()
+
+  reacher.finish()
+  ctx:screenshot()
+end
+
+local main = function(comparison, result_dir)
+  vim.o.runtimepath = vim.fn.getcwd() .. "," .. vim.o.runtimepath
+  vim.cmd("runtime! plugin/*.vim")
+
+  local test = require("virtes").setup({
+    scenario = scenario,
+    result_dir = result_dir,
+    cleanup = function()
+      vim.cmd("silent! %bwipeout!")
+      require("reacher.lib.module").cleanup()
+    end,
+  })
+  local before = test:run({hash = comparison})
+  before:write_replay_script()
+
+  local after = test:run({hash = nil})
+  after:write_replay_script()
+
+  before:diff(after):write_replay_script()
+end
+
+return main
