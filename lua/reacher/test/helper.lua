54436af1869f883a43a24b47d4cdf5efe79c7600
notomo <notomo.motono@gmail.com>
2023-02-03 09:37:22 +0900

    Adjust test.helper print

--- lua/reacher/test/helper.lua
+++ lua/reacher/test/helper.lua
@@ -8,7 +8,6 @@ function helper.before_each() end
 function helper.after_each()
   helper.cleanup()
   helper.cleanup_loaded_modules(plugin_name)
-  print(" ")
 end
 
 function helper.set_lines(lines)
